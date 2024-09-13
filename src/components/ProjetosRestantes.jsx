eb047ce30fe9b8442e1a8bc04c68df2ba8007746
DavidMarinho2203 <davidbeckhampaulo32@gmail.com>
2024-09-13 08:11:21 -0300

    fix: :bug: Corrigindo o botão de link para acessar o site do projeto

--- src/components/ProjetosRestantes.jsx
+++ src/components/ProjetosRestantes.jsx
@@ -7,10 +7,10 @@ export default function ProjetosRestantes() {
     return (
         <>
             {projetosRestante.map(projeto => (
-                <div key={projeto.nome} className="w-full mx-auto flex flex-col justify-center sm:p-2 bg-cinza rounded-xl">
-                    <div className="flex flex-col justify-center items-center gap-2">
+                <div key={projeto.nome} className="flex flex-col justify-center w-full mx-auto sm:p-2 bg-cinza rounded-xl">
+                    <div className="flex flex-col items-center justify-center gap-2">
                         <p className="text-base">Tecnologias usadas</p>
-                        <ul className="flex gap-3 justify-center items-center">
+                        <ul className="flex items-center justify-center gap-3">
                             {projeto.tecnologias.map(tec => (
                                 <i key={tec.key} className={`${tec.icone} text-2xl text-roxo`} title={tec.nome}>
                                 </i>
@@ -30,11 +30,11 @@ export default function ProjetosRestantes() {
                         {verProjeto[0] == projeto.nome
                             ? (
                                 <>
-                                    <div className="absolute w-full h-full top-0 bg-cinza50 flex flex-col justify-center items-center gap-1">
-                                        <p className="textshadomPretaBranco font-bold text-2xl ">{projeto.nomePrincipal}</p>
+                                    <div className="absolute top-0 flex flex-col items-center justify-center w-full h-full gap-1 bg-cinza50">
+                                        <p className="text-2xl font-bold textshadomPretaBranco ">{projeto.nomePrincipal}</p>
                                         <ul className="flex gap-8">
-                                        <a href={projeto.sit}  target="_blank" className="w-fit text-4xl px-2 py-1 rounded-2xl hover:bg-roxo hover:text-branco ri-pages-line" title="Site"></a>
-                                        <a href={projeto.github}  target="_blank" className="w-fit text-4xl px-2 py-1 rounded-2xl hover:bg-roxo hover:text-branco ri-code-s-slash-fill" title="Código"></a>
+                                        <a href={projeto.site}  target="_blank" className="px-2 py-1 text-4xl w-fit rounded-2xl hover:bg-roxo hover:text-branco ri-pages-line" title="Site"></a>
+                                        <a href={projeto.github}  target="_blank" className="px-2 py-1 text-4xl w-fit rounded-2xl hover:bg-roxo hover:text-branco ri-code-s-slash-fill" title="Código"></a>
                                         </ul>
                                     </div>
                                 </>
