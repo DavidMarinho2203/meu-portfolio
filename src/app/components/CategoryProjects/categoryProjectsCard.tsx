2704daa9705fe5bddf2795bc343ce2f5827e00f4
David Beckham <davidbeckhampaulo32@gmail.com>
2025-01-28 13:43:41 -0300

    feat: :art: Reparando estilizações

--- src/app/components/CategoryProjects/categoryProjectsCard.tsx
+++ src/app/components/CategoryProjects/categoryProjectsCard.tsx
@@ -4,37 +4,25 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const CategoryProjectsCard = ({ title, description, image, projects }: CategoryProjects) => (
-  <div className="bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-all transform hover:scale-105 duration-300">
+  <div className="bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-all transform hover:scale-105 duration-300 w-full sm:max-w-[300px] md:max-w-[400px]">
     <div className="relative">
       <Image src={image} alt={title} width={500} height={500}
-        className="w-full h-48 sm:h-56 object-cover transition-transform duration-300 hover:scale-110"
+        className="w-full h-48 sm:h-56 object-cover transition-transform duration-300 "
       />
 
-      <div className="absolute inset-0 bg-gradient-to-t from-black/50 to-transparent opacity-0 hover:opacity-100 transition-opacity duration-300">
-        <div className="absolute bottom-4 left-4 right-4">
-          <h3 className="text-xl font-bold text-white mb-2">{title}</h3>
-          <p className="text-gray-200 text-sm">{description}</p>
-        </div>
-      </div>
     </div>
-    <div className="p-6 h-[15rem] flex flex-col justify-between gap-5">
+    <div className="flex flex-col justify-between gap-5 p-5 h-64">
       <div>
         <h3 className="text-xl font-bold mb-2 text-gray-800">{title}</h3>
         <p className="text-gray-600 mb-4">{description}</p>
       </div>
 
       <div className="flex space-x-4">
-        {projects ? (
-          <Link
-            href={`/categoria/${title}`}
-            
-            className="flex gap-1 cursor-pointer hover:text-blue-600 duration-200 transition-all"
-          >
-            <Wrench />
-            Projetos
+        {projects && (
+          <Link href={`/categoria/${title}`} className="flex gap-1 cursor-pointer hover:text-blue-600 duration-200 transition-all" >
+             <Wrench /> Projetos
           </Link>
-        ) : (
-          <p className='flex items-center text-gray-600 hover:text-blue-600 transition-colors group gap-1 select-none'><Construction />Ainda não existe...</p>)}
+        )}
       </div>
     </div>
   </div>
--- src/app/components/CategoryProjects/index.tsx
+++ src/app/components/CategoryProjects/index.tsx
@@ -7,7 +7,7 @@ const CategoryProjects = () => {
     <section id="categoryProjects" className="py-20 bg-white">
       <div className="container mx-auto px-6">
         <h2 className="text-4xl font-bold text-center mb-12">Categorias de Projetos</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="flex flex-wrap justify-center items-center gap-8">
           {CategoryProjectsData.map((category, index) => (
             <CategoryProjectsCard key={index} {...category} />
           ))}
