ed9f1e46cd044f79cfcb34dfe44cfb412e369cb9
David Beckham <davidbeckhampaulo32@gmail.com>
2025-08-05 16:03:41 -0300

    chore: incluindo o projeto do Vocab Forge

--- src/data/Projetos.ts
+++ src/data/Projetos.ts
@@ -1,45 +1,53 @@
 const Projetos = [
-    {
-        title: 'Pedra,Papel, Tesoura',
-        description: 'Jogo de pedra, papel e tesoura',
-        image: '/img/resultados_dos_projetos/pedrapapeltesoura.webp',
-        site: "https://rock-paper-scissors-master-dun.vercel.app/",
-        github: "https://github.com/DavidMarinho2203/rock-paper-scissors-master",
-
-    },{
-        title: 'Interface extensões',
-        description: 'Interface do gerenciador de extensões',
-        image: '/img/resultados_dos_projetos/gerenciadorDeExtensoes.webp',
-        site: "https://browser-extension-manager-ui-psi.vercel.app/",
-        github: "https://github.com/DavidMarinho2203/browser-extension-manager-ui",
-
-    },{
-        title: 'Otimize Consultoria Jr',
-        description: 'Landing Page para empresa da Otimize',
-        image: '/img/resultados_dos_projetos/otimize.webp',
-        site: "https://otimizeconsultoriapb.netlify.app/",
-        github: "https://github.com/DavidMarinho2203/projeto-otimize",
-
-    },{
-        title: 'Bronzeamento',
-        description: 'Lading Page para uma empresa de bronzeamento',
-        image: '/img/resultados_dos_projetos/bronze.webp',
-        site: "https://karol-bronze.netlify.app/",
-        github: "https://github.com/DavidMarinho2203/projeto-karol-bronze",
-    }, {
-        title: 'Fotógrafo',
-        description: 'Lading Page para uma profissional da fotógrafia',
-        image: '/img/resultados_dos_projetos/fotografia.webp',
-        site: "https://projeto-para-fotografos.vercel.app/",
-        github: "https://github.com/DavidMarinho2203/projeto-para-fotografos",
-    }, {
-        title: 'Salgateria',
-        description: 'Landing Page para uma loja de salgados',
-        image: '/img/resultados_dos_projetos/salgateria.webp',
-        site: "https://reidossalgados.netlify.app/",
-        github: "https://github.com/DavidMarinho2203/projeto-rei-dos-salgados",
-    }, 
-
-]
+  {
+    title: "Vocab Forge",
+    description: "Plataforma de aprendizado de vocabulário",
+    image: "/img/resultados_dos_projetos/vocabforge02.webp",
+    site: "",
+    github: "https://github.com/DavidMarinho2203/vocab-forge",
+  },
+  {
+    title: "Pedra,Papel, Tesoura",
+    description: "Jogo de pedra, papel e tesoura",
+    image: "/img/resultados_dos_projetos/pedrapapeltesoura.webp",
+    site: "https://rock-paper-scissors-master-dun.vercel.app/",
+    github: "https://github.com/DavidMarinho2203/rock-paper-scissors-master",
+  },
+  {
+    title: "Interface extensões",
+    description: "Interface do gerenciador de extensões",
+    image: "/img/resultados_dos_projetos/gerenciadorDeExtensoes.webp",
+    site: "https://browser-extension-manager-ui-psi.vercel.app/",
+    github: "https://github.com/DavidMarinho2203/browser-extension-manager-ui",
+  },
+  {
+    title: "Otimize Consultoria Jr",
+    description: "Landing Page para empresa da Otimize",
+    image: "/img/resultados_dos_projetos/otimize.webp",
+    site: "https://otimizeconsultoriapb.netlify.app/",
+    github: "https://github.com/DavidMarinho2203/projeto-otimize",
+  },
+  {
+    title: "Bronzeamento",
+    description: "Lading Page para uma empresa de bronzeamento",
+    image: "/img/resultados_dos_projetos/bronze.webp",
+    site: "https://karol-bronze.netlify.app/",
+    github: "https://github.com/DavidMarinho2203/projeto-karol-bronze",
+  },
+  {
+    title: "Fotógrafo",
+    description: "Lading Page para uma profissional da fotógrafia",
+    image: "/img/resultados_dos_projetos/fotografia.webp",
+    site: "https://projeto-para-fotografos.vercel.app/",
+    github: "https://github.com/DavidMarinho2203/projeto-para-fotografos",
+  },
+  {
+    title: "Salgateria",
+    description: "Landing Page para uma loja de salgados",
+    image: "/img/resultados_dos_projetos/salgateria.webp",
+    site: "https://reidossalgados.netlify.app/",
+    github: "https://github.com/DavidMarinho2203/projeto-rei-dos-salgados",
+  },
+];
 
 export default Projetos;
